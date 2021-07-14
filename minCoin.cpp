0854f26ef1e51503bbea2aa590675cace651310d
mkk96 <one.end4@gmail.com>
2021-07-14 11:22:18 +0530

    MinCoin

--- minCoin.cpp
+++ minCoin.cpp
@@ -1,8 +1,9 @@
 #include<iostream>
+#include<bits/stdc++.h>
 using namespace std;
 int findmincoin(int *coin,int n,int m)
 {
-	int table[m+1],i,j;
+	int table[m+1],i,j,mincoinused[m+1];
 	table[0]=0;
 	for(i=1;i<=m;i++)
 	{
@@ -12,17 +13,19 @@ int findmincoin(int *coin,int n,int m)
 	{
 		for(j=0;j<n;j++)
 		{
-			if(coin[j]<=i)
+			if(i-coin[j]>=0&&(table[i-coin[j]]+1<table[i]))
 			{
-				int x=table[i-coin[j]];
-				if(x!=INT_MAX&&x+1<table[i])
-				{
-					table[i]=x+1;
-				}
+				table[i]=table[i-coin[j]]+1;
+				mincoinused[i]=coin[j];
 			}
 		}
 	}
-	
+	j=m;
+	while(j>0)
+	{
+		cout<<mincoinused[j]<<" ";
+		j-=mincoinused[j];
+	}
 	if(table[m]==INT_MAX)
 	{
 		return -1;
@@ -32,17 +35,17 @@ int findmincoin(int *coin,int n,int m)
 int main()
 {
 	int n;
-	cout<<"Enter the number of coin\t";
+	cout<<"Enter number of Coin\t";
 	cin>>n;
-	cout<<"Enter coins\n";
-	int i,coin[n],m;
+	int coin[n],i,m;
+	cout<<"Enter the Coin\n";
 	for(i=0;i<n;i++)
 	{
 		cin>>coin[i];
 	}
-	cout<<"Enter value\t";
+	cout<<"Enter the value\t";
 	cin>>m;
 	int mincoin=findmincoin(coin,n,m);
-	cout<<mincoin;
+	(mincoin==-1)?cout<<"No combination possible":cout<<mincoin;
 	return 0;
 }
